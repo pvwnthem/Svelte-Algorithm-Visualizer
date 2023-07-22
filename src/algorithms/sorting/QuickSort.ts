f62901c5581a172963ee3388543ff81c9ee06b22
pvwnthem <pvwn.them@gmail.com>
2023-07-22 19:16:06 -0400

    better quicksort visualization

--- src/algorithms/sorting/QuickSort.ts
+++ src/algorithms/sorting/QuickSort.ts
@@ -59,38 +59,43 @@ export default class QuickSort extends SortingAlgorithm {
           return [...QuickSort.run(left), pivot, ...QuickSort.run(right)];
     }
 
-    static async animate(array: number[], updateArray: (array: number[], index: number) => void, shouldStopSorting: () => boolean): Promise<number[]> {
-        const quickSort = async (arr: number[]): Promise<number[]> => {
-            if (arr.length <= 1) {
-                return arr;
+    static async animate(array: number[], updateArray: (array: number[], startIndex: number, endIndex: number) => void, shouldStopSorting: () => boolean): Promise<number[]> {
+    const quickSort = async (arr: number[], start: number, end: number): Promise<number[]> => {
+        if (end - start <= 0) {
+            return arr;
+        }
+        
+        const pivotIndex = Math.floor((start + end) / 2);
+        const pivot = arr[pivotIndex];
+        let left = start;
+        let right = end;
+        
+        while (left <= right) {
+            while (arr[left] < pivot) {
+                left++;
             }
             
-            const pivotIndex = Math.floor(arr.length / 2);
-            const pivot = arr[pivotIndex];
-            const left = [];
-            const right = [];
-            
-            for (let i = 0; i < arr.length; i++) {
-                if (i === pivotIndex) {
-                    continue;
-                }
-            
-                const current = arr[i];
-                if (current < pivot) {
-                    left.push(current);
-                } else {
-                    right.push(current);
-                }
+            while (arr[right] > pivot) {
+                right--;
             }
             
-            const sortedLeft = await quickSort(left);
-            updateArray([...sortedLeft, pivot, ...right], pivotIndex);
-            const sortedRight = await quickSort(right);
-            updateArray([...sortedLeft, pivot, ...sortedRight], pivotIndex);
-            return [...sortedLeft, pivot, ...sortedRight];
-        };
-    
-        const sortedArray = await quickSort(array);
-        return sortedArray;
-    }
+            if (left <= right) {
+                const temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                updateArray(arr, left, right);
+                await new Promise(resolve => setTimeout(resolve, 100));
+                left++;
+                right--;
+            }
+        }
+        
+        await quickSort(arr, start, right);
+        await quickSort(arr, left, end);
+        return arr;
+    };
+
+    const sortedArray = await quickSort(array, 0, array.length - 1);
+    return sortedArray;
+}
 }
